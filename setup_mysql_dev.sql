b633452546e4e85b5878399edeaa1c8660aa2097
26fati <fatibeautiful1@gmail.com>
2023-11-19 23:01:22 +0100

    6

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- create hbnb_dev_db user on localhost with password hbnb_dev_pwd
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY "hbnb_dev_pwd";
 
 -- grant all privileges on hbnb_dev_db for the hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
