45f2c02862b9d1280c5ce3384093ca7bf5124fb1
26fati <fatibeautiful1@gmail.com>
2023-11-21 12:32:45 +0100

    pycodestyle

--- models/amenity.py
+++ models/amenity.py
@@ -9,5 +9,6 @@ class Amenity(BaseModel, Base):
     """The Amenities of a place"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place',
-                                   secondary='place_amenity', viewonly=False, overlaps="amenities")
+    place_amenities = relationship(
+        'Place', secondary='place_amenity',
+        viewonly=False, overlaps="amenities")
