9cdf794a3ab9507edd93a9f739924b94670f4315
louzanizineddine <louzanizineddine@gmail.com>
2023-12-06 21:35:49 +0100

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
-# Script that sets up your web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
+apt-get update
+
+apt-get install -y nginx
 sudo ufw allow 'Nginx HTTP'
+
 mkdir -p /data/
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-content="
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>"
-echo "$content" > /data/web_static/releases/test/index.html 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+
+echo "<html>
+<head>
+</head>
+<body>
+Holberton School
+</body>
+</html>
+" > /data/web_static/releases/test/index.html
+
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+
 sed -i '57i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+
 service nginx restart
 
