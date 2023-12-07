dee0cba08298bca3990b2f4ae0d29f1eb03219a8
louzanizineddine <louzanizineddine@gmail.com>
2023-12-07 13:03:18 +0100

    task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from fabric.api import local, task, run, put
+from datetime import datetime
+import os.path
+
+
+@task
+def do_deploy(archive_path):
+    """deploy static archive to the servers web to servers"""
+
+    if not (os.path.exists(archive_path)):
+        return False
+    
+    try:
+        put(archive_path, "/tmp/")
+        fileName = archive_path.split('/')[-1]
+        fileNameWithNoExtension = fileName.split('.')[0]
+        path = '/data/web_static/releases/' + fileNameWithNoExtension
+        run('mkdir -p ' + path)
+        run('tar -xzf /tmp/' + fileName + ' -C ' + path)
+        run('rm /tmp/' + fileName)
+        run('mv ' + path + '/web_static/* ' + path)
+        run('rm -rf ' + path + '/web_static')
+        run('rm -rf /data/web_static/current')
+        run('ln -s ' + path + ' /data/web_static/current')
+        print('New version deployed!')
+        return True
+    except Exception:
+        return False
