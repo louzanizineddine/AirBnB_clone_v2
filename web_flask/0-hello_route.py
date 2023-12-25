d96eda42dcd55d6a1c9cc899fe7794f3e10802aa
louzanizineddine <louzanizineddine@gmail.com>
2023-12-25 15:26:43 +0100

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,10 @@
 #!/usr/bin/pyhton3
-"""This module starts the web flask application"""
+"""
+    This scripts starts the web flask application
+    listens on 0.0.0.0
+    port: 5000
+    debug mode: False
+"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,7 +12,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    '''display teh stirng Hello HBNB!'''
+    """display the string Hello HBNB!"""
     return "Hello HBNB!"
 
 
