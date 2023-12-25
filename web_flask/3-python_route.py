65da00ce33661599d0d42e4c8b562fbc7f9ef402
louzanizineddine <louzanizineddine@gmail.com>
2023-12-25 16:08:21 +0100

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/pyhton3
+"""
+    This scripts starts the web flask application
+    listens on 0.0.0.0
+    port: 5000
+    debug mode: False
+"""
+
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """display the string Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display the string HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """display what comes after /c/<text>"""
+    new_text = text.replace("_", " ")
+    return "C {}".format(new_text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_fun(text="is cool"):
+    """display what comes after /python/<text>"""
+    new_text = text.replace("_", " ")
+    return "Python {}".format(new_text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
