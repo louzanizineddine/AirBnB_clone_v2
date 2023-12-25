c2a93a624ae04ccac3fa848a86390cc9953046da
louzanizineddine <louzanizineddine@gmail.com>
2023-12-25 15:32:30 +0100

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/pyhton3
+"""
+    This scripts starts the web flask application
+    listens on 0.0.0.0
+    port: 5000
+    debug mode: False
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """display the string Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display the string HBNB"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
