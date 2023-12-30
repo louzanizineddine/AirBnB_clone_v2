e36b28946e71b6a071c606de770e6fdb45edf63c
louzanizineddine <louzanizineddine@gmail.com>
2023-12-30 22:18:50 +0100

    pycodestyle

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,11 +49,11 @@ def show_number(n):
     """Displays the number template n only if it's int otherwise 404"""
     return render_template("5-number.html", n=n)
 
+
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def odd_or_even(n):
     """Displays the number parity even or odd"""
     return render_template("6-number_odd_or_even.html", n=n)
-        
 
 
 if __name__ == "__main__":
